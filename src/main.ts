a1866fa7bd06ecc80c5b5394d75f1b1afcd8c8ff
Artur Bednarski <01005196@pw.edu.pl>
2025-02-21 22:58:01 +0100

    to deploy on render

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import * as express from 'express';
 import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Manually serve static files
+  // Enable CORS
+  app.enableCors({
+    origin: 'https://your-react-app.onrender.com', // Replace with your React app's URL on Render
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   app.use('/static', express.static(path.join(__dirname, '..', 'myreact', 'build', 'static')));
 
-  await app.listen(5000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 5000;
+
+  await app.listen(port);
 }
 bootstrap();
